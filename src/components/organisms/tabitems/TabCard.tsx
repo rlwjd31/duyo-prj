57b258eaf336d8432b966582609f516eb1c383ce
rlwjd31 <rlwjd31@naver.com>
2025-02-04 02:24:05 +0900

    🐛 [Bug]: animate-fade-in-slide-down triggered also on onclick event
    한 번의 click에도 double click시 발생해야 하는 애니메이션이 작동하는 오류가 발생

--- src/components/organisms/tabitems/TabCard.tsx
+++ src/components/organisms/tabitems/TabCard.tsx
@@ -10,6 +10,7 @@ export default function TabCard({ children }: { children: ReactNode }) {
     <div
       className={cn(
         "absolute mt-3 shadow-(--card-shadow) bg-white h-26 flex w-full items-center gap-x-1.5 rounded-lg px-1.5 py-2",
+        // @FIXME: double click이 아닌 click 이벤트 발생에도 animate-fade-in-slide-down이 발생함.
         isDropdownVisible
           ? "animate-(--animate-fade-in-slide-down)"
           : "animate-(--animate-fade-out-slide-up)"
