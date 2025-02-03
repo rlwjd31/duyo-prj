a9425722eaf5ff922bd1e7e4a22945e7206508a9
rlwjd31 <rlwjd31@naver.com>
2025-02-03 23:36:11 +0900

    ♻️ [Refactor]: Apply Tabcard to tab dropdown component

--- src/components/organisms/tabitems/FileTabDropdown.tsx
+++ src/components/organisms/tabitems/FileTabDropdown.tsx
@@ -1,23 +1,20 @@
 import Divider from "../../atoms/Divider";
 import IconsLabel from "../../molecules/IconsLabel";
+import TabCard from "./TabCard";
 
 export default function FileTabDropdown() {
   return (
-    // TODO: Container는 다른 atomic pattern level에서 분리.
-    <div className="absolute mt-3 shadow-(--card-shadow) bg-white h-26 flex w-full items-center gap-x-1.5 rounded-lg px-1.5 py-2">
+    <TabCard>
       <div className="flex h-full w-fit px-0.5 flex-col items-center justify-between opacity-100 transform-none">
         <div className="flex flex-row gap-x-0.5">
           <IconsLabel iconType="saveSvg" label="SVG로 저장" />
           <IconsLabel iconType="savePng" label="PNG로 저장" />
         </div>
-        <div className="select-none text-xs text-neutral-600">저장</div>
-      </div>
-      <Divider />
-      <div className="flex h-full w-fit px-0.5 flex-col items-center justify-between opacity-100 transform-none">
-        <div className="flex flex-row gap-x-0.5">
-          <IconsLabel iconType="cancel" label="실행취소" />
+        <div className="select-none text-xs text-neutral-600 opacity-90">
+          저장
         </div>
       </div>
-    </div>
+      <Divider />
+    </TabCard>
   );
 }
--- src/components/organisms/tabitems/HomeTabDropdown.tsx
+++ src/components/organisms/tabitems/HomeTabDropdown.tsx
@@ -1,10 +1,10 @@
 import Divider from "../../atoms/Divider";
 import IconsLabel from "../../molecules/IconsLabel";
+import TabCard from "./TabCard";
 
 export default function HomeTabDropdown() {
   return (
-    // TODO: Container는 다른 atomic pattern level에서 분리.
-    <div className="absolute mt-3 shadow-(--card-shadow) bg-white h-26 flex w-full items-center gap-x-1.5 rounded-lg px-1.5 py-2">
+    <TabCard>
       <div className="flex h-full w-fit px-0.5 flex-col items-center justify-between opacity-100 transform-none">
         <div className="flex flex-row gap-x-0.5">
           <IconsLabel iconType="cancel" />
@@ -77,6 +77,6 @@ export default function HomeTabDropdown() {
         </div>
       </div>
       <Divider />
-    </div>
+    </TabCard>
   );
 }
