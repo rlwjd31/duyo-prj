f87f08e947bbb412e75e9310d14457a20c7cf8ed
rlwjd31 <rlwjd31@naver.com>
2025-02-03 20:38:33 +0900

    🚀 [Feat]: Moving tab underline effect
    활성화된 tab에 따른 밑줄 효과 구현.
    밑줄이 tab전체 container내에서 움직여야 하므로 각 tab에 독립된 개체가 아닌 tab container 바로 아래의 계층구조를 이용하여 밑줄이 움직이는 효과를 구현.
    
    - 밑줄 움직이는 효과는 tab의 정확한 width를 알기 위해 tab dom에 접근하여 width와 offsetLeft를 구하여 구현함

--- src/TabNavigation.tsx
+++ src/TabNavigation.tsx
@@ -13,6 +13,18 @@ const BUTTON_PADDING = 8;
 
 export default function TabNavigation() {
   const [activeTabIndex, setActiveTabIndex] = useState(0);
+  const [underlineWidth, setUnderlineWidth] = useState(0);
+  const [underlineLeft, setUnderlineLeft] = useState(0);
+
+  const tabsRef = useRef<HTMLButtonElement[]>([]);
+
+  useEffect(() => {
+    const currentTab = tabsRef.current[activeTabIndex];
+    if (currentTab) {
+      setUnderlineLeft(currentTab.offsetLeft - BUTTON_PADDING / 2);
+      setUnderlineWidth(currentTab.clientWidth - BUTTON_PADDING);
+    }
+  }, [activeTabIndex]);
 
   return (
     <div>
@@ -34,11 +46,18 @@ export default function TabNavigation() {
                   paddingRight: BUTTON_PADDING,
                 }}
                 onClick={() => setActiveTabIndex(idx)}
+                ref={(el) => (tabsRef.current[idx] = el as HTMLButtonElement)}
               >
                 {content}
               </button>
             );
           })}
+        <div className="absolute bottom-0 left-0 right-0 mx-2">
+          <span
+            className="absolute bottom-0 left-0 block h-0.5 bg-neutral-700 transition-all duration-250 ease-in"
+            style={{ left: underlineLeft, width: underlineWidth }}
+          />
+        </div>
       </div>
     </div>
   );
