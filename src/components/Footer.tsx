d0a2bb701546bf2387720bae2ac8b176f7f94582
rlwjd31 <rlwjd31@naver.com>
2025-02-05 08:04:51 +0900

    🚀 [Feat]: Scale up and down when on click minus and plus button
    마이너스 또는 플러스 버튼 클릭 시 scale 조절

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,17 +6,29 @@ export default function Footer() {
   const { currentScale, setCurrentScale } = useScale();
 
   return (
-    <footer className="absolute bottom-0 left-0 z-20 justify-end flex h-6 w-full flex-row items-center border-t border-neutral-300 px-4">
+    <footer className="z-20 justify-end flex h-6 w-full flex-row items-center border-t border-neutral-300 px-4">
       <div className="flex h-full flex-row items-center gap-x-2">
         <div className="flex flex-row gap-x-1.5">
-          <button className="h-full px-3 outline-none hover:bg-neutral-200">
+          <button
+            className="h-full px-3 outline-none hover:bg-neutral-200 cursor-pointer"
+            onClick={() =>
+              setCurrentScale((prev) =>
+                prev - 0.02 <= 0.1 ? 0.1 : prev - 0.02
+              )
+            }
+          >
             <Icon iconType="minus" />
           </button>
           <CustomInputRange
             value={+(currentScale * 100).toFixed(0)}
             setValue={setCurrentScale}
           />
-          <button className="h-full px-3 outline-none hover:bg-neutral-200">
+          <button
+            className="h-full px-3 outline-none hover:bg-neutral-200 cursor-pointer"
+            onClick={() =>
+              setCurrentScale((prev) => (prev + 0.02 >= 1 ? 1 : prev + 0.02))
+            }
+          >
             <Icon iconType="plus" />
           </button>
         </div>
