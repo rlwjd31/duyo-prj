9af1c4cfcb3f784173047ca1d00776abecfcef51
rlwjd31 <rlwjd31@naver.com>
2025-02-05 10:36:03 +0900

    ♻️ [Refactor]: Remove duplicated random code

--- src/hooks/generateRandomPolygon.tsx
+++ src/hooks/generateRandomPolygon.tsx
@@ -4,14 +4,16 @@ import { useShapes } from "../providers/Shapes.provider";
 export default function useGenerateRandomPolygon() {
   const { setShapes } = useShapes();
 
+  const x = Math.floor(Math.random() * (1400 - -300 + 1)) - 300; // (max - (-min) + 1) - min
+  const y = Math.floor(Math.random() * (1000 - -300 + 1)) - 300;
+  const borderColor =
+    randomColors[Math.floor(Math.random() * randomColors.length)];
+  const backgroundColor =
+    randomColors[Math.floor(Math.random() * randomColors.length)];
+
   const generateRandomCircle = () => {
-    const x = Math.floor(Math.random() * (1800 - -200 + 1)) - 200; // (max - (-min) + 1) - min
-    const y = Math.floor(Math.random() * (1200 - -200 + 1)) - 200;
     const size = Math.floor(Math.random() * (400 - 200 + 1)) + 200;
-    const borderColor =
-      randomColors[Math.floor(Math.random() * randomColors.length)];
-    const backgroundColor =
-      randomColors[Math.floor(Math.random() * randomColors.length)];
+
     setShapes((prev) => [
       ...prev,
       {
@@ -26,14 +28,8 @@ export default function useGenerateRandomPolygon() {
     ]);
   };
   const generateRandomRectangle = () => {
-    const x = Math.floor(Math.random() * (1400 - -300 + 1)) - 300; // (max - (-min) + 1) - min
-    const y = Math.floor(Math.random() * (1000 - -300 + 1)) - 300;
     const width = Math.floor(Math.random() * (400 - 200 + 1)) + 200;
     const height = Math.floor(Math.random() * (400 - 200 + 1)) + 200;
-    const borderColor =
-      randomColors[Math.floor(Math.random() * randomColors.length)];
-    const backgroundColor =
-      randomColors[Math.floor(Math.random() * randomColors.length)];
 
     setShapes((prev) => [
       ...prev,
